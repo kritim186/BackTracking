146a2de271145eb27166df92e3a78aeb47797c32
kritim186 <kritim186@gmail.com>
2025-02-06 22:51:19 +0530

    queens

--- Nqueens.java
+++ Nqueens.java
@@ -4,11 +4,35 @@ public class Nqueens {
             printB(board);
             return;
         }
-        for(int i = 0; i < board.length; i++){          
+        for(int i = 0; i < board.length; i++){  
+            if(isSafe(board, row, i)){        
                 board[row][i] = 'Q';
-                nQueens(board, row+1);
-                board[row][i]='.';
+                nQueens(board, row+1);//recursion call
+                board[row][i]='.';//backtracking
             }
+            }
+    }
+    public static boolean isSafe(char board[][], int row, int col){
+
+        //vertically up
+        for(int i = row-1; i >= 0; i--){
+            if(board[i][col] == 'Q'){
+                return false;
+            }
+        }
+        // Left Diagonal Up
+        for (int i = row-1, j=col-1; i >= 0 && j>=0; i--, j--) {
+            if(board[i][j] == 'Q'){
+                    return false;
+            }
+        }
+        //Right Diagonal Up
+        for (int i = row-1, j = col+1; i >= 0 && j < board.length; i--,j++) {
+        if(board[i][j] == 'Q'){
+            return false;
+        } 
+    }
+    return true;   
     }
     public static void printB(char board[][]){
         System.out.println("---------------");
@@ -22,7 +46,7 @@ public class Nqueens {
 
     public static void main(String[] args) {
         
-        int n = 2;
+        int n = 4;
         char board[][] = new char[n][n];
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
